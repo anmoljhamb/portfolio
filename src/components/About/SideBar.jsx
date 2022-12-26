af93244d8cdbe8a63851542b2ebc336692129f0f
Anmol Jhamb <talktoanmol@outlook.com>
2022-12-27 01:25:32 +0530

    sidebar href links

--- src/components/About/SideBar.jsx
+++ src/components/About/SideBar.jsx
@@ -1,31 +1,45 @@
 import React from "react";
+import {
+    twitterLink,
+    githubLink,
+    instagramLink,
+    linkedInLink,
+} from "../../constants";
 
 const SideBar = () => {
     return (
         <div className="sideBar">
             <li>
-                <i
-                    className="fa-brands fa-twitter fa-5x"
-                    style={{ backgroundColor: "#1DA1F2" }}
-                ></i>
+                <a href={twitterLink} target="_blank">
+                    <i
+                        className="fa-brands fa-twitter fa-5x"
+                        style={{ backgroundColor: "#1DA1F2" }}
+                    ></i>
+                </a>
             </li>
             <li>
-                <i
-                    className="fa-brands fa-github fa-5x"
-                    style={{ backgroundColor: "#333" }}
-                ></i>
+                <a href={githubLink} target="_blank">
+                    <i
+                        className="fa-brands fa-github fa-5x"
+                        style={{ backgroundColor: "#333" }}
+                    ></i>
+                </a>
             </li>
             <li>
-                <i
-                    className="fa-brands fa-linkedin fa-5x"
-                    style={{ backgroundColor: "#0A66C2" }}
-                ></i>
+                <a href={linkedInLink} target="_blank">
+                    <i
+                        className="fa-brands fa-linkedin fa-5x"
+                        style={{ backgroundColor: "#0A66C2" }}
+                    ></i>
+                </a>
             </li>
             <li>
-                <i
-                    className="fa-brands fa-instagram fa-5x"
-                    style={{ backgroundColor: "#e95950" }}
-                ></i>
+                <a href={instagramLink} target="_blank">
+                    <i
+                        className="fa-brands fa-instagram fa-5x"
+                        style={{ backgroundColor: "#e95950" }}
+                    ></i>
+                </a>
             </li>
         </div>
     );
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,8 @@
 export const loadingColor = "#8758ff";
 export const pageloadingSize = 150;
 export const appLoadingSize = 80;
+export const twitterLink = "https://twitter.com/heyitsanmolj";
+export const githubLink = "https://github.com/anmoljhamb";
+export const instagramLink = "https://instagram.com/heyitsanmolj";
+export const linkedInLink =
+    "https://www.linkedin.com/in/anmol-jhamb-16270925b/";
