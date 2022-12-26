97477326c2320c46bf2c10449f08cff9351ad83f
Anmol Jhamb <talktoanmol@outlook.com>
2022-12-26 17:01:37 +0530

    helloWorld

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.scss";
+
+function App() {
+    /**
+     * * App will have multiple pages, but no navbar. Just left and right swipes. On Home Page, it will have a sidebar of my linkedin, github and instagram. On the right side, I will have a right icon. Default is the about page.
+     *
+     *
+     * * / - About
+     * * / Portfolio All the projects, worth showing.
+     * * / Utils. Just some stuff I built for my own use.
+     * * / Contact. Really important tho. Must mail me the stuff.
+     * * Loading animation on page
+     * * Top Bar slide show.
+     * * Fade in out.
+     *
+     */
+
+    return (
+        <section className="flex_c">
+            <h1>Hello World!</h1>
+        </section>
+    );
+}
+
+export default App;
