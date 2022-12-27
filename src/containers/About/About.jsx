3f6b77794a7ffcb19c4fe53df954e3728fe31037
Anmol Jhamb <talktoanmol@outlook.com>
2022-12-27 14:08:09 +0530

    aboutPage text added, with highlight trasnition

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -15,12 +15,36 @@ const About = () => {
     return (
         <IfLoading>
             <SideBar></SideBar>
-            <section>
+            <section style={{ border: "0rem" }}>
                 <div className="content">
                     <h1>Hello, I am Anmol Jhamb</h1>
                     <p>
-                        hello how you <span className="highlight">doing</span>{" "}
-                        sir??
+                        I am a 20-year-old tech enthusiast. I had been coding
+                        for a while before I got to pursue it as a career. I am
+                        a{" "}
+                        <span className="highlight">
+                            Google-Code-in 2019 Grand Prize Winner.
+                        </span>{" "}
+                        I have experience working in{" "}
+                        <span className="highlight">
+                            Full Stack Development
+                        </span>
+                        , <span className="highlight"> Python Scripting</span>,
+                        <span className="highlight">
+                            {" "}
+                            Android App Development
+                        </span>
+                        , and <span className="highlight">IoT</span>. I am
+                        currently looking to find a good remote internship that
+                        would allow me the chance to work with a team and get
+                        real-world experience.
+                        <br />
+                        Checkout the <span className="highlight">
+                            projects
+                        </span>{" "}
+                        that I've workd on. Or the{" "}
+                        <div className="highlight">Utilities</div> I made to
+                        make my life easier.
                     </p>
                 </div>
             </section>
