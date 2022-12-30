6a6157d8b89836df859cb257744fe325481f43b7
Anmol Jhamb <talktoanmol@outlook.com>
2022-12-30 12:15:35 +0530

    subHeader of about section

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,2 @@
 export { default as codingGif } from "./codingGif.gif";
+export { default as avatar } from "./avatar.png";
--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,7 +1,8 @@
 import React, { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { IfLoading, NextArrow, Particle, SideBar } from "../../components";
 import Context from "../../context/Context";
-import { codingGif } from "../../assets";
+import { avatar, codingGif } from "../../assets";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import { colorVariables } from "../../constants";
 import { motion } from "framer-motion";
@@ -18,14 +19,18 @@ const About = () => {
 
     const [text, count] = useTypewriter({
         words: [
-            "Full Stack",
-            "IoT",
-            "Python Scripting",
-            "Android App Development",
+            "<Full Stack />",
+            "<IoT />",
+            "<Python Scripting />",
+            "<Android App Development />",
         ],
         loop: true,
         delaySpeed: 1200,
     });
+
+    const handleOnClick = (event) => {
+        setLoading(true);
+    };
 
     return (
         <IfLoading>
@@ -76,33 +81,79 @@ const About = () => {
                         </motion.div>
                     </div>
                     <div className="sub_header">
-                        <p>this is my sub header</p>
+                        <h1>
+                            Let me <span className="colored">introduce</span>{" "}
+                            myself
+                        </h1>
+                        <div className="container">
+                            <div className="avatar">
+                                <img src={avatar} alt="codingAvatar" />
+                            </div>
+                            <div className="content">
+                                I am a 20-year-old tech enthusiast. I had been
+                                coding for a while before I got to pursue it as
+                                a career. I am a{" "}
+                                <Link
+                                    onClick={handleOnClick}
+                                    to="/portfolio"
+                                    className="highlight"
+                                >
+                                    Google Code-in Grand Prize Winner.
+                                </Link>{" "}
+                                I have experience working with{" "}
+                                <Link
+                                    onClick={handleOnClick}
+                                    to="/portfolio"
+                                    className="highlight"
+                                >
+                                    Full Stack Development
+                                </Link>
+                                ,{" "}
+                                <Link
+                                    onClick={handleOnClick}
+                                    to="/portfolio"
+                                    className="highlight"
+                                >
+                                    {" "}
+                                    Python Scripting
+                                </Link>
+                                ,
+                                <Link
+                                    onClick={handleOnClick}
+                                    to="/portfolio"
+                                    className="highlight"
+                                >
+                                    {" "}
+                                    Android App Development
+                                </Link>
+                                , and{" "}
+                                <Link
+                                    onClick={handleOnClick}
+                                    to="/portfolio"
+                                    className="highlight"
+                                >
+                                    IoT
+                                </Link>
+                                . Checkout the{" "}
+                                <Link
+                                    onClick={handleOnClick}
+                                    to="/portfolio"
+                                    className="highlight"
+                                >
+                                    Projects
+                                </Link>{" "}
+                                that I've workd on. Or the{" "}
+                                <Link
+                                    onClick={handleOnClick}
+                                    to="/portfolio"
+                                    className="highlight"
+                                >
+                                    Utilities
+                                </Link>{" "}
+                                I made to make my life easier.
+                            </div>
+                        </div>
                     </div>
-                    {/* <p>
-                        I am a 20-year-old tech enthusiast. I had been coding
-                        for a while before I got to pursue it as a career. I am
-                        a{" "}
-                        <span className="highlight">
-                            Google-Code-in 2019 Grand Prize Winner.
-                        </span>{" "}
-                        I have experience working in{" "}
-                        <span className="highlight">
-                            Full Stack Development
-                        </span>
-                        , <span className="highlight"> Python Scripting</span>,
-                        <span className="highlight">
-                            {" "}
-                            Android App Development
-                        </span>
-                        , and <span className="highlight">IoT</span>. I am
-                        currently looking to find a good remote internship that
-                        would allow me the chance to work with a team and get
-                        real-world experience. Checkout the{" "}
-                        <span className="highlight">projects</span> that I've
-                        workd on. Or the{" "}
-                        <span className="highlight">Utilities</span> I made to
-                        make my life easier.
-                    </p> */}
                 </header>
             </section>
             <NextArrow next="/portfolio"></NextArrow>
