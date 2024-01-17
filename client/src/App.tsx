a13bf81e1c0d4ff0ab3441e2688d4f5970d0a3f5
Anmol Jhamb <talktoanmol@outlook.com>
2024-01-17 11:38:13 +0530

    Added tailwind.css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,7 @@
-import React, { Suspense } from "react";
-
-const UserProfile = React.lazy(() => import("./SuspenseExample"));
-
 function App() {
   return (
     <>
-      <div>
-        <h1>Hello World!!</h1>
-      </div>
-      <Suspense fallback="Loading...">
-        <UserProfile />
-      </Suspense>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 }
