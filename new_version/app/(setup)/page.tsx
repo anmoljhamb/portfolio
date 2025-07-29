6aca9c0314cb72296799ff000f770370c840ef02
Anmol Jhamb <talktoanmol@outlook.com>
2025-07-29 20:10:00 +0530

    change the loading strategy

--- new_version/app/(setup)/page.tsx
+++ new_version/app/(setup)/page.tsx
@@ -1,8 +1,20 @@
-import { delay } from "@/app/utils/misc";
-import { redirect } from "next/navigation";
+"use client";
 
-export default async function Home() {
-  await delay(2000);
-  redirect("/about");
-  return <div>Home Page</div>;
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
+import { GridLoader } from "react-spinners";
+import { APP_LOADING_TIMER } from "@/app/constants";
+
+export default function Home() {
+  useEffect(() => {
+    setTimeout(() => {
+      redirect("/about");
+    }, APP_LOADING_TIMER);
+  }, []);
+
+  return (
+    <div className="w-screen h-screen bg-dark flex justify-center items-center">
+      <GridLoader loading size={90} speedMultiplier={0.8} color="#f05454" />
+    </div>
+  );
 }
