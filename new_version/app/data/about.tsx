a7f3777ac446e525dc20ae959949175bfb0546c6
Anmol Jhamb <talktoanmol@outlook.com>
2025-08-01 03:02:43 +0530

    AboutPage fixed

--- new_version/app/data/about.tsx
+++ new_version/app/data/about.tsx
@@ -1,10 +1,24 @@
+import React from "react";
+
 export const FULL_NAME = "Anmol Jhamb";
-export const TAGS = ["SWE", "ICPC Regionalist '24", "Google Code In 2019"];
+
+export const TAGS = [
+  "Software Engineer",
+  "Problem Solver",
+  "ICPC Regionalist '24",
+  "Google Code-in Winner '19",
+];
 
 export const SHORT_SUMMARY = (
   <>
-    I love building full-stack products end-to-end, from sketch to ship.
-    <br />I use <span className="text-accent font-semibold inline">NeoVim</span>
-    , btw.
+    I am a developer driven by a passion for turning complex challenges into
+    elegant, end-to-end solutions. My journey started with a teenage dream of
+    building my own AI and has evolved into a career focused on crafting
+    impactful software. I thrive on the entire creation process, from a
+    back-of-the-napkin idea to a polished, shipped product.
+    <br />
+    And yes, I build it all from the comfort of{" "}
+    <span className="text-accent font-semibold inline">Neovim</span>, on a
+    custom-tuned Debian environment.
   </>
 );
